58fde66f74ec3aceeebfe4688d51c3769ea1986b
Xeftdayc <Xeftdayc@XEFTDAYC>
2018-12-23 15:48:27 -0500

    Pull2

--- Worknesh/Worknesh.py
+++ Worknesh/Worknesh.py
@@ -1,3 +1,28 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import Menu
+import sqlite3
+
+#---Conexion Base de Datos---
+iConn = sqlite3.connect("workneshdb.sqlite")
+iCur = iConn.cursos()
+
+#---Creacion de tablas---
+try:
+    iCur.execute ('''
+        create table tDatos (
+        id integer primary key autoincrement,
+        dni int(8) not null,
+        nombre varchar(50),
+        apellidos varchar(100),
+        nromovil int(9),
+        email varchar(100),
+        direccion varchar(100),
+        ''')
+
+#---Salir base de datos
+
+
 #------Primera Capa
 
 print("Primera prueba para el test v1")
